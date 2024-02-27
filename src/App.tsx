e2da8b169ecc4e4511995e2eca01d7f58312bf14
crypto-tesco <jesco_bee@protonmail.com>
2024-02-27 14:39:17 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [clicks, setClicks] = useState<number>(0);
+
+  function isPrime(num: number): boolean {
+  // Handle invalid input (non-positive numbers)
+  if (num <= 1) {
+    return false;
+  }
+  if (num === 2) {
+    return true;
+  }
+
+  const limit = Math.sqrt(num);
+  for (let i = 2; i <= limit; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+  }
+
+  useEffect(() => {
+    if(clicks > 0) {
+      if(isPrime(clicks)) {
+        console.error(`The prime ${clicks} has been clicked!`)
+      } else {
+        console.log(`Clicked the number: ${clicks}!`)
+      }
+    }
+  }, [clicks])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={"main"}>
+      <div className={"container"}>
+        <div className={"header"}>
+          Heroku Logplex Demo
+        </div>
+        <div className={"description"}>
+          This is an example app to showcase the awesomeness of Heroku Logplex.
+        </div>
+        <div className={"description"}>
+          Hit the button to count and see the magic of logplex unfold!
+        </div>
+        <div className={"buttonContainer"}>
+        <button className={"button"} onClick={() => setClicks((prev: number) => prev+1)}>
+          <div className={"buttonText"}>
+          Click Me!
+          </div>
+        </button>
+        <div className={"description"}>
+          The button has been clicked: {clicks} times.
+        </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
